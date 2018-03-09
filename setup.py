2ffdf96693d85dd0c9f0ab05cd856d6924f41d72
Katy Barnhart <katy.barnhart@gmail.com>
2018-03-09 15:06:01 -0700

    removing install_requires

--- setup.py
+++ setup.py
@@ -14,9 +14,4 @@ setup(name='terrainbento',
       license='MIT',
       packages=['terrainbento'],
       long_description=open('README.md').read(),
-      zip_safe=False,
-      install_requires=['scipy>=0.12',
-                        'dill',
-                        'numpy',
-                        'landlab']
-                        )
+      zip_safe=False])
