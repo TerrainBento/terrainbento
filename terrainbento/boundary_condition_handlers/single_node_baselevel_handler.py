1c7a583bb473255dc42af6d65cb949c06341520e
kbarnhart <katy.barnhart@gmail.com>
2018-03-26 15:19:28 -0600

    fixing indentation

--- terrainbento/boundary_condition_handlers/single_node_baselevel_handler.py
+++ terrainbento/boundary_condition_handlers/single_node_baselevel_handler.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-single_node_baselevel_handler.py: controls elevation for a single open
-boundary node.
-
-Created on Wed Nov 15 10:36:07 2017
-
-@author: gtucker
+SingleNodeBaselevelHandler ontrols elevation for a single open boundary node.
 """
+
 import numpy as np
 from scipy.interpolate import interp1d
+
 
 class SingleNodeBaselevelHandler():
     """SingleNodeBaselevelHandler controls elevation for a single open
@@ -35,27 +32,27 @@ class SingleNodeBaselevelHandler():
                  model_end_elevation = None,
                  **kwargs):
 
-    self.grid = grid
-
-    self.outlet_lowering_rate = self.params.get('outlet_lowering_rate',  0.0)
-
-    try:
-        file_name = self.params['outlet_lowering_file_path']
-
-        model_end_elevation = self.params['model_end_elevation']
-
-        model_start_elevation = self.z[self.outlet_node]
-
-        elev_change_df = np.loadtxt(file_name, skiprows=1, delimiter =',')
-        time = elev_change_df[:, 0]
-        elev_change = elev_change_df[:, 1]
-        scaling_factor = np.abs(model_start_elevation-model_end_elevation)/np.abs(elev_change[0]-elev_change[-1])
-        outlet_elevation = (scaling_factor*elev_change_df[:, 1]) + model_start_elevation
-
-        self.outlet_elevation_obj = interp1d(time, outlet_elevation)
-
-    except KeyError:
-        self.outlet_elevation_obj = None
+        self.grid = grid
+    
+        self.outlet_lowering_rate = self.params.get('outlet_lowering_rate',  0.0)
+    
+        try:
+            file_name = self.params['outlet_lowering_file_path']
+    
+            model_end_elevation = self.params['model_end_elevation']
+    
+            model_start_elevation = self.z[self.outlet_node]
+    
+            elev_change_df = np.loadtxt(file_name, skiprows=1, delimiter =',')
+            time = elev_change_df[:, 0]
+            elev_change = elev_change_df[:, 1]
+            scaling_factor = np.abs(model_start_elevation-model_end_elevation)/np.abs(elev_change[0]-elev_change[-1])
+            outlet_elevation = (scaling_factor*elev_change_df[:, 1]) + model_start_elevation
+    
+            self.outlet_elevation_obj = interp1d(time, outlet_elevation)
+    
+        except KeyError:
+            self.outlet_elevation_obj = None
 
     def run_one_step(self, dt):
         """
