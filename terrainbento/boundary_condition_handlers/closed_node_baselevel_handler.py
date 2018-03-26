c4d2abe5c251763a4e57e7f8ee49604a35b1bd1a
kbarnhart <katy.barnhart@gmail.com>
2018-03-26 13:45:47 -0600

    changing single node baselevel handler into two baselevel handlers

--- terrainbento/boundary_condition_handlers/closed_node_baselevel_handler.py
+++ terrainbento/boundary_condition_handlers/closed_node_baselevel_handler.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-single_node_baselevel_handler.py: controls elevation for a single open
-boundary node.
-
-Created on Wed Nov 15 10:36:07 2017
-
-@author: gtucker
+closed_node_baselevel_handler.py: controls elevation for all closed nodes.
 """
 import numpy as np
 from scipy.interpolate import interp1d
 
-class SingleNodeBaselevelHandler():
-    """SingleNodeBaselevelHandler controls elevation for a single open
-    boundary node, referred to here as the *outlet*. The outlet lowering rate is
+class ClosedNodeBaselevelHandler():
+    """ClosedNodeBaselevelHandler controls elevation for closed nodes.
+
+    The outlet lowering rate is
     specified either as a constant or through a time, elevation change textfile.
 
     Parameters
     ----------
     grid
-    outlet_node
+    modify_boundary_nodes
     outlet_lowering_rate
     outlet_lowering_file_path
     modern_outlet_elevation
@@ -29,7 +25,7 @@ class SingleNodeBaselevelHandler():
 
     def __init__(self,
                  grid,
-                 outlet_node,
+                 modify_boundary_nodes = False,
                  outlet_lowering_rate = 0.0,
                  outlet_lowering_file_path = None,
                  model_end_elevation = None,
@@ -63,18 +59,23 @@ class SingleNodeBaselevelHandler():
         """
 
         # determine which nodes to lower
-        nodes_to_lower = self.outlet_node
+        if self.modify_boundary_nodes:
+            nodes_to_lower = self.grid.status_at_node != 0
+            prefactor = -1.0
+        else:
+            nodes_to_lower = self.grid.status_at_node == 0
+            prefactor = 1.0
 
         # next, lower the correct nodes the desired amount
 
         # first, if we do not have an outlet elevation object
         if self.outlet_elevation_obj is None:
 
-            # calculate lowering amount and subtract
-            self.z[nodes_to_lower] -= self.outlet_lowering_rate * dt
+            # if this is not a watershed, we are raising the core nodes
+            self.z[nodes_to_lower] += prefactor * self.outlet_lowering_rate * dt
 
             # if bedrock_elevation exists as a field, lower it also
-            self.grid.at_node['bedrock__elevation'][nodes_to_lower] -= self.outlet_lowering_rate * dt
+            self.grid.at_node['bedrock__elevation'][nodes_to_lower] += prefactor * self.outlet_lowering_rate * dt
 
         # if there is an outlet elevation object
         else:
@@ -82,9 +83,12 @@ class SingleNodeBaselevelHandler():
             # calcuate the topographic change required to match the current time's value for
             # outlet elevation. This must be done in case bedrock elevation exists, and must
             # be done before the topography is lowered
+
+            topo_change = prefactor * (self.z[nodes_to_lower] - self.outlet_elevation_obj(self.model_time))
+
             if 'bedrock__elevation' in self.grid.at_node.keys():
-                topo_change = self.z[nodes_to_lower] - self.outlet_elevation_obj(self.model_time)
-                self.grid.at_node['bedrock__elevation'][nodes_to_lower] -= topo_change
+
+                self.grid.at_node['bedrock__elevation'][nodes_to_lower] += topo_change
 
             # lower topography
-            self.z[nodes_to_lower] = self.outlet_elevation_obj(self.model_time)
+            self.z[nodes_to_lower] += topo_change
