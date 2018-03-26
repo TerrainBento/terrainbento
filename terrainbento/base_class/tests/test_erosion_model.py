9f6a2769aafb9bf2a9d50c99bc41d0435fb4d778
kbarnhart <katy.barnhart@gmail.com>
2018-03-26 10:55:47 -0600

    placeholder for some tests

---
+++ terrainbento/base_class/tests/test_erosion_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 09:58:16 2018
+
+@author: barnhark
+"""
+
+def test_no_inputs():
+    """Test providing neither input file nor parameters."""
+    pass
+
+def test_both_inputs():
+    """Test providing both input file and parameters."""
+    pass
+
+def test_input_file():
+    """Test providing input file."""
+    pass
+
+def test_parameters():
+    """Test providing parameters file."""
+    pass
+
+def test_load_from_pickle():
+    """Test providing parameters file."""
+    pass
+
+def test_create_pickle():
+    """Test providing parameters file."""
+    pass
+
+"""Test HexModelGrid."""
+    pass
+
+"""Test RasterModelGrid."""
+    pass
+
+"""Test DEM."""
+    pass
---
+++ terrainbento/base_class/tests/test_stochastic_erosion_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 26 09:58:16 2018
+
+@author: barnhark
+"""
+
+def test_no_inputs():
+    """Test providing neither input file nor parameters."""   
+    pass
+
+
+def test_both_inputs():
+    """Test providing both input file and parameters."""
+    pass
+
+
+def test_input_file():
+    """Test providing input file."""
+    pass
+
+def test_parameters():
+    """Test providing parameters file."""
+    pass
