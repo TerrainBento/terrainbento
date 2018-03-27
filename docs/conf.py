d60a65e737a8c475b9592cd61c3d0272938b2b61
kbarnhart <katy.barnhart@gmail.com>
2018-03-27 11:47:27 -0600

    mocking as instructed by RTFD
    http://docs.readthedocs.io/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,17 @@
 #
 import os
 import sys
+
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['landlab', 'dill', 'numpy', 'scipy', 'yaml']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 import terrainbento
 
 autodoc_mock_imports = ['landlab', 'dill', 'numpy', 'scipy', 'yaml']
