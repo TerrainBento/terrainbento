d6dd99f0a3bfd0079f5b85239a2f345c982d7631
kbarnhart <katy.barnhart@gmail.com>
2018-03-27 20:41:41 -0600

    making coverage simpler

--- scripts/test-installed-terrainbento.py
+++ scripts/test-installed-terrainbento.py
@@ -1,25 +1,15 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
-import sys, os
+import sys
+import os
+import coverage
+
 
 sys.path.pop(0)
 
-from optparse import OptionParser
-
-parser = OptionParser('usage: %prog [options] -- [nosetests options]')
-parser.add_option('-v', '--verbose', action='count', dest='verbose',
-                  default=1, help='increase verbosity [%default]')
-parser.add_option('--no-doctests', action='store_false', dest='doctests',
-                  default=True,
-                  help='Do not run doctests in module [%default]')
-parser.add_option('--coverage', action='store_true', dest='coverage',
-                  default=False, help='report coverage of landlab [%default]')
-parser.add_option('-m', '--mode', action='store', dest='mode', default='fast',
-                  help='"fast", "full", or something that can be passed to '
-                       'nosetests -A [%default]')
-
-(options, args) = parser.parse_args()
+cov = coverage.Coverage()
+cov.start()
 
 try:
     import terrainbento
@@ -30,9 +20,10 @@ except ImportError:
     raise
 
 
-result = terrainbento.test(label=options.mode, verbose=options.verbose,
-                      doctests=options.doctests, coverage=options.coverage,
-                      extra_argv=args, raise_warnings='release')
+result = terrainbento.test()
+
+cov.stop()
+cov.save()
 
 if result.wasSuccessful():
     sys.exit(0)
