fe5952e8faa1d8f285bd7fc3000d247a20f77ae2
kbarnhart <katy.barnhart@gmail.com>
2018-05-07 18:06:22 -0600

    adding some extra imports

--- terrainbento/base_class/tests/test_erosion_model_flow_opts.py
+++ terrainbento/base_class/tests/test_erosion_model_flow_opts.py
@@ -1,3 +1,11 @@
+import numpy as np
+#from numpy.testing import assert_array_equal, assert_array_almost_equal
+from nose.tools import assert_raises#, assert_almost_equal, assert_equal
+
+from landlab import HexModelGrid
+from terrainbento import ErosionModel
+
+
 def test_FlowAccumulator_options():
     """Test FlowAccumulator instantiation."""
     # D8, D4, MFD x hex, raster, dem
--- terrainbento/base_class/tests/test_erosion_model_inputs.py
+++ terrainbento/base_class/tests/test_erosion_model_inputs.py
@@ -1,9 +1,12 @@
+import os
 import numpy as np
 #from numpy.testing import assert_array_equal, assert_array_almost_equal
 from nose.tools import assert_raises#, assert_almost_equal, assert_equal
 
 from landlab import HexModelGrid
 from terrainbento import ErosionModel
+
+_TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def test_no_inputs():
