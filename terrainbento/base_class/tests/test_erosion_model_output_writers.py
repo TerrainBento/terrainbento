413a02e1a7ca11a2e05c979028e9c7df6550600e
kbarnhart <katy.barnhart@gmail.com>
2018-06-09 23:28:34 -0600

    remove unnecessary import

--- terrainbento/base_class/tests/test_erosion_model_output_writers.py
+++ terrainbento/base_class/tests/test_erosion_model_output_writers.py
@@ -3,7 +3,6 @@ import numpy as np
 #from numpy.testing import assert_array_equal, assert_array_almost_equal
 from nose.tools import assert_raises#, assert_almost_equal, assert_equal
 
-from landlab import HexModelGrid
 from terrainbento import ErosionModel
 
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
