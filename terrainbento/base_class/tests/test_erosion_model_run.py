da1c69b84a7fa97c9924f9d281ed2de151faa222
kbarnhart <katy.barnhart@gmail.com>
2018-06-09 22:25:34 -0600

    run basic test

--- terrainbento/base_class/tests/test_erosion_model_run.py
+++ terrainbento/base_class/tests/test_erosion_model_run.py
@@ -1,13 +1,18 @@
+import os
 import numpy as np
 #from numpy.testing import assert_array_equal, assert_array_almost_equal
 from nose.tools import assert_raises#, assert_almost_equal, assert_equal
 
 from landlab import HexModelGrid
-from terrainbento import ErosionModel
+from terrainbento import Basic
 
+_TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 def test_run_for():
-    pass
+    fp = os.path.join(_TEST_DATA_DIR, 'basic_inputs.txt')
+    model = Basic(input_file=fp)
+    model.run_for(10., 100.)
+    assert model.model_time == 100.
 
 def test_finalize():
     pass
