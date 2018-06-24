26cc753602ac1b6abe3989818ba4f70705396ca7
kbarnhart <katy.barnhart@gmail.com>
2018-06-23 21:16:23 -0600

    unique test file names
    since this seems to be an issue for appveyor

--- tests/test_erosion_model_outputs.py
+++ tests/test_erosion_model_outputs.py
@@ -16,8 +16,9 @@ _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 def test_write_output_raster():
     fp = os.path.join(_TEST_DATA_DIR, 'basic_raster_inputs.txt')
     model = Basic(input_file=fp)
+    model._out_file_name ='tb_raster_output'
     model.run()
-    fs = glob.glob('terrainbento_output*.nc')
+    fs = glob.glob(model._out_file_name + '*.nc')
 
     assert len(fs) == 5
 
@@ -32,8 +33,9 @@ def test_write_output_raster():
 def test_write_output_hex():
     fp = os.path.join(_TEST_DATA_DIR, 'basic_inputs.txt')
     model = Basic(input_file=fp)
+    model._out_file_name ='tb_hex_output'
     model.run()
-    fs = glob.glob('terrainbento_output*.nc')
+    fs = glob.glob(model._out_file_name + '*.nc')
 
     assert len(fs) == 5
     #ds = xr.open_dataset(fs[0])
