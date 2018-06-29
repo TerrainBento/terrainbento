5241a20e47e2b13a251c0cf9e1ec162ba8f24576
cmshobe <cmshobe@email.wm.edu>
2018-06-29 09:26:39 -0600

    adding first couple tests for 410

---
+++ tests/test_410.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+
+from numpy.testing import assert_array_almost_equal # assert_array_equal,
+import pytest
+
+from landlab import HexModelGrid
+from terrainbento import BasicHySa
+
+
+def test_no_Ksp_or_Kss():
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'regolith_transport_parameter': 0.001}
+
+    pytest.raises(ValueError, BasicHySa, params=params)
+
+
+def test_both_Ksp_or_Kss():
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'regolith_transport_parameter': 0.001,
+              'water_erodability': 0.001,
+              'water_erodability~shear_stress': 0.001}
+    pytest.raises(ValueError, BasicHySa, params=params)
+    
