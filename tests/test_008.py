cfa7685f8fc288211c695b1192db4d582a40e228
cmshobe <cmshobe@email.wm.edu>
2018-07-02 10:40:26 -0600

    adding kss test to 008

--- tests/test_008.py
+++ tests/test_008.py
@@ -32,6 +32,50 @@ def test_both_Ksp_or_Kss():
               'thresh_change_per_depth': 0.001}
     pytest.raises(ValueError, BasicDd, params=params)
 
+def test_steady_Kss_no_precip_changer():
+    U = 0.0001
+    K = 0.001
+    m = 1./3.
+    n = 1.
+    dt = 1000
+    threshold = 0.000001
+    thresh_change_per_depth = 0
+    # construct dictionary. note that D is turned off here
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'number_of_node_rows' : 3,
+              'number_of_node_columns' : 20,
+              'node_spacing' : 100.0,
+              'north_boundary_closed': True,
+              'south_boundary_closed': True,
+              'regolith_transport_parameter': 0.,
+              'water_erodability~shear_stress': K,
+              'erosion__threshold': threshold,
+              'thresh_change_per_depth': thresh_change_per_depth,
+              'm_sp': m,
+              'n_sp': n,
+              'random_seed': 3141,
+              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
+                                              'lowering_rate': -U}}
+
+    # construct and run model
+    model = BasicDd(params=params)
+    for i in range(100):
+        model.run_one_step(dt)
+
+    # construct actual and predicted slopes
+    actual_slopes = model.grid.at_node['topographic__steepest_slope']
+    actual_areas = model.grid.at_node['drainage_area']
+    predicted_slopes = ((U/K + threshold)/((actual_areas**m))) ** (1./n)
+
+    # assert actual and predicted slopes are the same.
+    assert_array_almost_equal(actual_slopes[model.grid.core_nodes[1:-1]],
+                              predicted_slopes[model.grid.core_nodes[1:-1]],
+                              decimal=4)
+
 def test_steady_Ksp_no_precip_changer():
     U = 0.0001
     K = 0.001
