44ee1e75c7a2138f7e3c16adfbc7a93fabb2a66c
cmshobe <cmshobe@email.wm.edu>
2018-07-02 11:08:59 -0600

    adding kss test for 010

--- tests/test_010.py
+++ tests/test_010.py
@@ -27,6 +27,56 @@ def test_both_Ksp_or_Kss():
               'water_erodability': 0.001,
               'water_erodability~shear_stress': 0.001}
     pytest.raises(ValueError, BasicHy, params=params)
+    
+def test_steady_Kss_no_precip_changer():
+    U = 0.0001
+    K = 0.003
+    m = 1./3.
+    n = 1.
+    dt = 10
+    v_sc = 0.001
+    phi = 0.1
+    F_f = 0.0
+    # construct dictionary. note that D is turned off here
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'number_of_node_rows' : 3,
+              'number_of_node_columns' : 20,
+              'node_spacing' : 100.0,
+              'north_boundary_closed': True,
+              'south_boundary_closed': True,
+              'regolith_transport_parameter': 0.,
+              'water_erodability~shear_stress': K,
+              'v_sc': v_sc,
+              'phi': phi,
+              'F_f': F_f,
+              'solver': 'basic',
+              'm_sp': m,
+              'n_sp': n,
+              'random_seed': 3141,
+              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
+                                              'lowering_rate': -U}}
+
+    # construct and run model
+    model = BasicHy(params=params)
+    for i in range(2000):
+        model.run_one_step(dt)
+
+    # construct actual and predicted slopes
+    actual_slopes = model.grid.at_node['topographic__steepest_slope']
+    actual_areas = model.grid.at_node['drainage_area']
+    predicted_slopes =  (np.power(((U * v_sc) / (K
+        * np.power(actual_areas, m)))
+        + (U / (K * np.power(actual_areas,
+        m))), 1./n))
+
+    # assert actual and predicted slopes are the same.
+    assert_array_almost_equal(actual_slopes[model.grid.core_nodes[1:-1]],
+                              predicted_slopes[model.grid.core_nodes[1:-1]],
+                              decimal=4)
 
 def test_steady_Ksp_no_precip_changer():
     U = 0.0001
