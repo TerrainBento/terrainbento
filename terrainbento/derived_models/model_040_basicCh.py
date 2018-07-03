2dd972226622e43213fc691ea11059b84d1c3205
Rachel Glade <rachel.glade@colorado.edu>
2018-07-03 11:26:45 -0600

    docs for 040

--- terrainbento/derived_models/model_040_basicCh.py
+++ terrainbento/derived_models/model_040_basicCh.py
@@ -1,7 +1,7 @@
 #! /usr/env/python
 """ ``terrainbento`` Model ``BasicCh`` program.
 
-Erosion model using cubic diffusion, basic stream
+Erosion model program using cubic diffusion, basic stream
 power, and discharge proportional to drainage area.
 
 
@@ -28,16 +28,82 @@ class BasicCh(ErosionModel):
 
     .. math::
 
-        
+        \\frac{\partial \eta}{\partial t} = -K_{w}A^{m}S^{n} + nabla^2 q_s
 
-    A BasicCh computes erosion using cubic diffusion, basic stream
-    power, and Q~A.
+    where
+
+    :math::
+
+        \q_s = DS(1+(\\frac{S}{S_c}^2 + \\frac{S}{S_c}^4) + .. + (\frac{S}{S_c}^{2(n-1)})
+
+    where :math: `S_c` is the critical slope. Refer to the ``terrainbento`` manuscript Table XX (URL here) for parameter
+    symbols, names, and dimensions.
+
+    Model ``BasicCh`` inherits from the ``terrainbento`` ``ErosionModel`` base
+    class.
     """
 
     def __init__(
         self, input_file=None, params=None, BoundaryHandlers=None, OutputWriters=None
     ):
-        """Initialize the BasicCh."""
+        """
+        Parameters
+        ----------
+        input_file : str
+            Path to model input file. See wiki for discussion of input file
+            formatting. One of input_file or params is required.
+        params : dict
+            Dictionary containing the input file. One of input_file or params is
+            required.
+        BoundaryHandlers : class or list of classes, optional
+            Classes used to handle boundary conditions. Alternatively can be
+            passed by input file as string. Valid options described above.
+        OutputWriters : class, function, or list of classes and/or functions, optional
+            Classes or functions used to write incremental output (e.g. make a
+            diagnostic plot).
+
+        Returns
+        -------
+        BasicCh : model object
+        
+        Examples
+        --------
+        This is a minimal example to demonstrate how to construct an instance
+        of model ``BasicCh``. Note that a YAML input file can be used instead of
+        a parameter dictionary. For more detailed examples, including steady-
+        state test examples, see the ``terrainbento`` tutorials.
+
+        To begin, import the model class.
+
+        >>> from terrainbento import BasicCh
+
+        Set up a parameters variable.
+
+        >>> params = {'model_grid': 'RasterModelGrid',
+        ...           'dt': 1,
+        ...           'output_interval': 2.,
+        ...           'run_duration': 200.,
+        ...           'number_of_node_rows' : 6,
+        ...           'number_of_node_columns' : 9,
+        ...           'node_spacing' : 10.0,
+        ...           'regolith_transport_parameter': 0.001,
+                      'slope_crit': 0.2,
+        ...           'water_erodability': 0.001,
+        ...           'm_sp': 0.5,
+        ...           'n_sp': 1.0}
+
+        Construct the model.
+
+        >>> model = BasicCh(params=params)
+
+        Running the model with ``model.run()`` would create output, so here we
+        will just run it one step.
+
+        >>> model.run_one_step(1.)
+        >>> model.model_time
+        1.0
+
+        """
 
         # Call ErosionModel's init
         super(BasicCh, self).__init__(
@@ -72,8 +138,30 @@ class BasicCh(ErosionModel):
         )
 
     def run_one_step(self, dt):
-        """
-        Advance model for one time-step of duration dt.
+        """Advance model ``BasicCh`` for one time-step of duration dt.
+
+        The **run_one_step** method does the following:
+
+        1. Directs flow and accumulates drainage area.
+
+        2. Assesses the location, if any, of flooded nodes where erosion should
+        not occur.
+
+        3. Assesses if a ``PrecipChanger`` is an active BoundaryHandler and if
+        so, uses it to modify the erodability by water.
+
+        4. Calculates detachment-limited erosion by water.
+
+        5. Calculates topographic change by nonlinear diffusion.
+
+        6. Finalizes the step using the ``ErosionModel`` base class function
+        **finalize__run_one_step**. This function updates all BoundaryHandlers
+        by ``dt`` and increments model time by ``dt``.
+
+        Parameters
+        ----------
+        dt : float
+            Increment of time for which the model is run.
         """
 
         # Route flow
