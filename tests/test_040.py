b3e74cb90035676d56c6a1ffbe58f5fb1167e77d
Rachel Glade <rachel.glade@colorado.edu>
2018-07-06 17:03:44 -0600

    buggies

--- tests/test_040.py
+++ tests/test_040.py
@@ -7,6 +7,7 @@ import pytest
 
 from landlab import HexModelGrid
 from terrainbento import BasicCh
+from terrainbento import Basic
 
 
 def test_diffusion_only():
@@ -15,7 +16,7 @@ def test_diffusion_only():
     m = 0.5
     n = 1.0
     dt = 10
-    D = 0.001
+    D = 0.01
     S_c = 0.2
     dx = 10.0
     runtime = 100000
@@ -60,6 +61,49 @@ def test_diffusion_only():
     actual_slope = np.abs(model.grid.at_node['topographic__steepest_slope'][39])
     assert_array_almost_equal(actual_slope, predicted_slope)
 
+# def test_steady_Ksp_no_precip_changer_with_depression_finding():
+#     U = 0.0001
+#     K = 0.001
+#     m = 0.5
+#     n = 1.0
+#     dt = 1.0
+#     run_time = 800
+#     # construct dictionary. note that D is turned off here
+#     params = {'model_grid': 'RasterModelGrid',
+#               'dt': dt,
+#               'output_interval': 2.,
+#               'run_duration': 200.,
+#               'number_of_node_rows' : 3,
+#               'number_of_node_columns' : 20,
+#               'node_spacing' : 100.0,
+#               'north_boundary_closed': True,
+#               'south_boundary_closed': True,
+#               'regolith_transport_parameter': 0.0,
+#               'water_erodability': K,
+#               'slope_crit': 0.0,
+#               'm_sp': m,
+#               'n_sp': n,
+#               'random_seed': 3141,
+#               'depression_finder': 'DepressionFinderAndRouter',
+#               'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+#               'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
+#                                               'lowering_rate': -U}}
+    
+#     # construct and run model
+#     model = BasicCh(params=params)
+#     for i in range(run_time):
+#         model.run_one_step(dt)
+
+#     # construct actual and predicted slopes
+#     actual_slopes = model.grid.at_node['topographic__steepest_slope']
+#     actual_areas = model.grid.at_node['drainage_area']
+#     predicted_slopes = (U/(K * (actual_areas**m))) ** (1./n)
+
+#     # assert actual and predicted slopes are the same.
+#     assert_array_almost_equal(actual_slopes[model.grid.core_nodes[1:-1]],
+#                               predicted_slopes[model.grid.core_nodes[1:-1]])
+
+
 def test_steady_Ksp_no_precip_changer_with_depression_finding():
     U = 0.0001
     K = 0.001
@@ -79,7 +123,6 @@ def test_steady_Ksp_no_precip_changer_with_depression_finding():
               'south_boundary_closed': True,
               'regolith_transport_parameter': 0.0,
               'water_erodability': K,
-              'slope_crit': 0.0,
               'm_sp': m,
               'n_sp': n,
               'random_seed': 3141,
@@ -89,7 +132,7 @@ def test_steady_Ksp_no_precip_changer_with_depression_finding():
                                               'lowering_rate': -U}}
     
     # construct and run model
-    model = BasicCh(params=params)
+    model = Basic(params=params)
     for i in range(run_time):
         model.run_one_step(dt)
 
