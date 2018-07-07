6e979ce7805b4eb3bcb203b6ac62f525a6454c8e
Rachel Glade <rachel.glade@colorado.edu>
2018-07-07 14:35:26 -0600

    bugs

--- tests/test_040.py
+++ tests/test_040.py
@@ -15,11 +15,11 @@ def test_diffusion_only():
     K = 0.0
     m = 0.5
     n = 1.0
-    dt = 10
+    dt = 1
     D = 0.001
     S_c = 0.2
     dx = 10.0
-    runtime = 1000000
+    runtime = 100000
 
     #Construct dictionary. Note that stream power is turned off
     params = {'model_grid': 'RasterModelGrid',
@@ -33,9 +33,9 @@ def test_diffusion_only():
                 'water_erodability': K,
                 'm_sp': m,
                 'n_sp': n,
-          	  'slope_crit': S_c,
-          	  'depression_finder': 'DepressionFinderAndRouter',
-          	  'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+          	    'slope_crit': S_c,
+          	    'depression_finder': 'DepressionFinderAndRouter',
+          	    'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
                 'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
                                                 'lowering_rate': -U}}
 
@@ -60,6 +60,8 @@ def test_diffusion_only():
 
     actual_slope = np.abs(model.grid.at_node['topographic__steepest_slope'][39])
     assert_array_almost_equal(actual_slope, predicted_slope)
+
+
 
 def test_steady_Ksp_no_precip_changer_with_depression_finding():
     U = 0.0001
@@ -105,11 +107,76 @@ def test_steady_Ksp_no_precip_changer_with_depression_finding():
 
 
 
+def test_steady_Ksp_no_precip_changer():
+    U = 0.0001
+    K = 0.001
+    m = 0.5
+    n = 1.0
+    dt = 1000
+    # construct dictionary. note that D is turned off here
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'number_of_node_rows' : 3,
+              'number_of_node_columns' : 20,
+              'node_spacing' : 100.0,
+              'north_boundary_closed': True,
+              'south_boundary_closed': True,
+              'regolith_transport_parameter': 0.,
+              'slope_crit': 0.0,
+              'water_erodability': K,
+              'm_sp': m,
+              'n_sp': n,
+              'random_seed': 3141,
+              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
+                                              'lowering_rate': -U}}
+
+    # construct and run model
+    model = BasicCh(params=params)
+    for i in range(100):
+        model.run_one_step(dt)
+
+    # construct actual and predicted slopes
+    actual_slopes = model.grid.at_node['topographic__steepest_slope']
+    actual_areas = model.grid.at_node['drainage_area']
+    predicted_slopes = (U/(K * (actual_areas**m))) ** (1./n)
+
+    # assert actual and predicted slopes are the same.
+    assert_array_almost_equal(actual_slopes[model.grid.core_nodes[1:-1]],
+                              predicted_slopes[model.grid.core_nodes[1:-1]])
 
 
+def test_with_precip_changer():
+    K =  0.01
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'number_of_node_rows' : 3,
+              'number_of_node_columns' : 20,
+              'node_spacing' : 100.0,
+              'north_boundary_closed': True,
+              'south_boundary_closed': True,
+              'regolith_transport_parameter': 0.,
+              'slope_crit': 0.0,
+              'water_erodability': K,
+              'm_sp': 0.5,
+              'n_sp': 1.0,
+              'random_seed': 3141,
+              'BoundaryHandlers': 'PrecipChanger',
+              'PrecipChanger' : {'daily_rainfall__intermittency_factor': 0.5,
+                                 'daily_rainfall__intermittency_factor_time_rate_of_change': 0.1,
+                                 'daily_rainfall__mean_intensity': 1.0,
+                                 'daily_rainfall__mean_intensity_time_rate_of_change': 0.2}}
 
-
-
+    model = BasicCh(params=params)
+    assert model.eroder.K == K
+    assert 'PrecipChanger' in model.boundary_handler
+    model.run_one_step(1.0)
+    model.run_one_step(1.0)
+    assert round(model.eroder.K, 5) == 0.10326
 
 
 
