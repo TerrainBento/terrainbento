93c8f3d5919f77e5d36751956c2788d3450f61ec
kbarnhart <katy.barnhart@gmail.com>
2018-07-10 15:39:38 -0600

    passing 802 tests

--- tests/test_802.py
+++ tests/test_802.py
@@ -11,117 +11,12 @@ from terrainbento import BasicRtTh
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
-def test_no_Ksp_or_Kss_rock():
-    params = {
-        "model_grid": "RasterModelGrid",
-        "dt": 1,
-        "output_interval": 2.,
-        "run_duration": 200.,
-        "contact_zone__width": 1.0,
-        "regolith_transport_parameter": 0.001,
-        "water_erodability~upper": 0.005,
-    }
-
-    pytest.raises(ValueError, BasicRtTh, params=params)
-
-
-def test_both_Ksp_or_Kss_rock():
-    params = {
-        "model_grid": "RasterModelGrid",
-        "dt": 1,
-        "output_interval": 2.,
-        "run_duration": 200.,
-        "contact_zone__width": 1.0,
-        "regolith_transport_parameter": 0.001,
-        "water_erodability~lower": 0.001,
-        "water_erodability~lower~shear_stress": 0.001,
-        "water_erodability~upper": 0.005,
-    }
-    pytest.raises(ValueError, BasicRtTh, params=params)
-
-
-def test_no_Ksp_or_Kss_till():
-    params = {
-        "model_grid": "RasterModelGrid",
-        "dt": 1,
-        "output_interval": 2.,
-        "run_duration": 200.,
-        "contact_zone__width": 1.0,
-        "regolith_transport_parameter": 0.001,
-        "water_erodability~lower": 0.005,
-    }
-
-    pytest.raises(ValueError, BasicRtTh, params=params)
-
-
-def test_both_Ksp_or_Kss_till():
-    params = {
-        "model_grid": "RasterModelGrid",
-        "dt": 1,
-        "output_interval": 2.,
-        "run_duration": 200.,
-        "regolith_transport_parameter": 0.001,
-        "contact_zone__width": 1.0,
-        "water_erodability~lower": 0.001,
-        "water_erodability~upper": 0.005,
-        "water_erodability~upper~shear_stress": 0.005,
-    }
-    pytest.raises(ValueError, BasicRtTh, params=params)
-
-
-def test_steady_Kss_no_precip_changer():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 1. / 3.
-    n = 2. / 3.
-    dt = 1000
-
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.txt")
-    # construct dictionary. note that D is turned off here
-    params = {
-        "model_grid": "RasterModelGrid",
-        "dt": 1,
-        "output_interval": 2.,
-        "run_duration": 200.,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
-        "regolith_transport_parameter": 0.,
-        "water_erodability~lower~shear_stress": Kr,
-        "water_erodability~upper~shear_stress": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {"modify_core_nodes": True, "lowering_rate": -U},
-    }
-
-    # construct and run model
-    model = BasicRtTh(params=params)
-    for i in range(100):
-        model.run_one_step(dt)
-
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
-
-    # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[22:37], rock_predicted_slopes[22:37])
-
-    # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[82:97], till_predicted_slopes[82:97])
-
-
 def test_steady_Ksp_no_precip_changer():
     U = 0.0001
     Kr = 0.001
     Kt = 0.005
+    Tr = 0.0001
+    Tt = 0.0005
     m = 0.5
     n = 1.0
     dt = 1000
@@ -139,8 +34,10 @@ def test_steady_Ksp_no_precip_changer():
         "north_boundary_closed": True,
         "south_boundary_closed": True,
         "regolith_transport_parameter": 0.,
-        "water_erodability~lower~shear_stress": Kr,
-        "water_erodability~upper~shear_stress": Kt,
+        "water_erodability~lower": Kr,
+        "water_erodability~upper": Kt,
+        'water_erosion_rule~upper~threshold___parameter': Tt,
+        'water_erosion_rule~lower~threshold___parameter': Tr,
         "lithology_contact_elevation__file_name": file_name,
         "contact_zone__width": 1.,
         "m_sp": m,
@@ -152,27 +49,37 @@ def test_steady_Ksp_no_precip_changer():
 
     # construct and run model
     model = BasicRtTh(params=params)
-    for i in range(100):
+    for i in range(200):
         model.run_one_step(dt)
 
     actual_slopes = model.grid.at_node["topographic__steepest_slope"]
     actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
+
+    # note that since we have a smooth threshold, we do not have a true
+    # analytical solution, but a bracket within wich we expect the actual slopes
+    # to fall.
+    rock_predicted_slopes_upper = ((U + Tr) / (Kr * (actual_areas ** m))) ** (1. / n)
+    till_predicted_slopes_upper = ((U + Tt) / (Kt * (actual_areas ** m))) ** (1. / n)
+
+    rock_predicted_slopes_lower = ((U + 0.) / (Kr * (actual_areas ** m))) ** (1. / n)
+    till_predicted_slopes_lower = ((U + 0.) / (Kt * (actual_areas ** m))) ** (1. / n)
 
     # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[22:37], rock_predicted_slopes[22:37])
+    assert np.all(actual_slopes[22:37] > rock_predicted_slopes_lower[22:37]) == True
+    assert np.all(actual_slopes[22:37] < rock_predicted_slopes_upper[22:37]) == True
 
-    # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[82:97], till_predicted_slopes[82:97])
+    assert np.all(actual_slopes[82:97] > till_predicted_slopes_lower[82:97]) == True
+    assert np.all(actual_slopes[82:97] < till_predicted_slopes_upper[82:97]) == True
 
 
 def test_steady_Ksp_no_precip_changer_with_depression_finding():
     U = 0.0001
     Kr = 0.001
     Kt = 0.005
-    m = 1. / 3.
-    n = 2. / 3.
+    Tr = 0.001
+    Tt = 0.005
+    m = 0.5
+    n = 1.0
     dt = 1000
 
     file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.txt")
@@ -188,45 +95,57 @@ def test_steady_Ksp_no_precip_changer_with_depression_finding():
         "north_boundary_closed": True,
         "south_boundary_closed": True,
         "regolith_transport_parameter": 0.,
-        "water_erodability~lower~shear_stress": Kr,
-        "water_erodability~upper~shear_stress": Kt,
+        "water_erodability~lower": Kr,
+        "water_erodability~upper": Kt,
+        'water_erosion_rule~upper~threshold___parameter': Tt,
+        'water_erosion_rule~lower~threshold___parameter': Tr,
         "lithology_contact_elevation__file_name": file_name,
         "contact_zone__width": 1.,
         "m_sp": m,
         "n_sp": n,
         "random_seed": 3141,
-        "depression_finder": "DepressionFinderAndRouter",
         "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
+        "depression_finder": "DepressionFinderAndRouter",
         "NotCoreNodeBaselevelHandler": {"modify_core_nodes": True, "lowering_rate": -U},
     }
 
     # construct and run model
     model = BasicRtTh(params=params)
-    for i in range(100):
+    for i in range(200):
         model.run_one_step(dt)
 
     actual_slopes = model.grid.at_node["topographic__steepest_slope"]
     actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
+
+    # note that since we have a smooth threshold, we do not have a true
+    # analytical solution, but a bracket within wich we expect the actual slopes
+    # to fall.
+    rock_predicted_slopes_upper = ((U + Tr) / (Kr * (actual_areas ** m))) ** (1. / n)
+    till_predicted_slopes_upper = ((U + Tt) / (Kt * (actual_areas ** m))) ** (1. / n)
+
+    rock_predicted_slopes_lower = ((U + 0.) / (Kr * (actual_areas ** m))) ** (1. / n)
+    till_predicted_slopes_lower = ((U + 0.) / (Kt * (actual_areas ** m))) ** (1. / n)
 
     # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[22:37], rock_predicted_slopes[22:37])
+    assert np.all(actual_slopes[22:37] > rock_predicted_slopes_lower[22:37]) == True
+    assert np.all(actual_slopes[22:37] < rock_predicted_slopes_upper[22:37]) == True
 
-    # assert actual and predicted slopes are the same for rock and till portions.
-    assert_array_almost_equal(actual_slopes[82:97], till_predicted_slopes[82:97])
+    assert np.all(actual_slopes[82:97] > till_predicted_slopes_lower[82:97]) == True
+    assert np.all(actual_slopes[82:97] < till_predicted_slopes_upper[82:97]) == True
 
 
 def test_diffusion_only():
-
     total_time = 5.0e6
-    U = 0.001
-    D = 1
-    m = 0.75
+    U = 0.0001
+    Kr = 0.
+    Kt = 0.
+    Tr = 0.000001
+    Tt = 0.000001
+    m = 0.5
     n = 1.0
     dt = 1000
+    D = 1
 
-    # construct dictionary. note that D is turned off here
     file_name = os.path.join(_TEST_DATA_DIR, "example_contact_diffusion.txt")
     # construct dictionary. note that D is turned off here
     params = {
@@ -240,8 +159,10 @@ def test_diffusion_only():
         "north_boundary_closed": True,
         "south_boundary_closed": True,
         "regolith_transport_parameter": D,
-        "water_erodability~lower~shear_stress": 0,
-        "water_erodability~upper~shear_stress": 0,
+        "water_erodability~lower": Kr,
+        "water_erodability~upper": Kt,
+        'water_erosion_rule~upper~threshold___parameter': Tt,
+        'water_erosion_rule~lower~threshold___parameter': Tr,
         "lithology_contact_elevation__file_name": file_name,
         "contact_zone__width": 1.,
         "m_sp": m,
@@ -273,10 +194,17 @@ def test_diffusion_only():
 
 
 def test_with_precip_changer():
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_diffusion.txt")
+    U = 0.0001
+    Kr = 0.001
+    Kt = 0.005
+    Tr = 0.01
+    Tt = 0.05
+    m = 0.5
+    n = 1.0
+    dt = 1000
 
-    Kr = 0.01
-    Kt = 0.001
+    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_diffusion.txt")
+    # construct dictionary. note that D is turned off here
     params = {
         "model_grid": "RasterModelGrid",
         "dt": 1,
@@ -288,8 +216,10 @@ def test_with_precip_changer():
         "north_boundary_closed": True,
         "south_boundary_closed": True,
         "regolith_transport_parameter": 0.,
-        "water_erodability~lower~shear_stress": Kr,
-        "water_erodability~upper~shear_stress": Kt,
+        "water_erodability~lower": Kr,
+        "water_erodability~upper": Kt,
+        'water_erosion_rule~upper~threshold___parameter': Tt,
+        'water_erosion_rule~lower~threshold___parameter': Tr,
         "lithology_contact_elevation__file_name": file_name,
         "contact_zone__width": 1.,
         "m_sp": 0.5,
@@ -305,7 +235,7 @@ def test_with_precip_changer():
     }
 
     model = BasicRtTh(params=params)
-    model._update_erodability_field()
+    model._update_erodability_and_threshold_fields()
     assert np.array_equiv(model.eroder.K[model.grid.core_nodes[:8]], Kt) == True
     assert np.array_equiv(model.eroder.K[model.grid.core_nodes[10:]], Kr) == True
 
