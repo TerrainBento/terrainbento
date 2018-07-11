9f0a7560204e66df01158a0c8bfa107abaa2060d
kbarnhart <katy.barnhart@gmail.com>
2018-07-10 18:39:49 -0600

    wip

--- tests/test_002.py
+++ tests/test_002.py
@@ -8,72 +8,17 @@ from landlab import HexModelGrid
 from terrainbento import BasicTh
 
 
-def test_no_Ksp_or_Kss():
+def bad_n_sp():
     params = {'model_grid': 'RasterModelGrid',
               'dt': 1,
               'output_interval': 2.,
               'run_duration': 200.,
               'erosion__threshold': 0.001,
+              'water_erodability': 0.001,
+              'n_sp': 1.01,
               'regolith_transport_parameter': 0.001}
 
     pytest.raises(ValueError, BasicTh, params=params)
-
-
-def test_both_Ksp_or_Kss():
-    params = {'model_grid': 'RasterModelGrid',
-              'dt': 1,
-              'output_interval': 2.,
-              'run_duration': 200.,
-              'erosion__threshold': 0.001,
-              'regolith_transport_parameter': 0.001,
-              'water_erodability': 0.001,
-              'water_erodability~shear_stress': 0.001}
-    pytest.raises(ValueError, BasicTh, params=params)
-    
-def test_steady_Kss_no_precip_changer():
-    U = 0.0001
-    K = 0.001
-    m = 1. / 3.
-    n = 2. / 3.
-    dt = 1000
-    threshold = 0.001
-    # construct dictionary. note that D is turned off here
-    params = {'model_grid': 'RasterModelGrid',
-              'dt': 1,
-              'output_interval': 2.,
-              'run_duration': 200.,
-              'number_of_node_rows' : 3,
-              'number_of_node_columns' : 20,
-              'node_spacing' : 100.0,
-              'north_boundary_closed': True,
-              'south_boundary_closed': True,
-              'regolith_transport_parameter': 0.,
-              'water_erodability~shear_stress': K,
-              'erosion__threshold': threshold,
-              'm_sp': m,
-              'n_sp': n,
-              'random_seed': 3141,
-              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
-              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
-                                              'lowering_rate': -U}}
-
-    # construct and run model
-    model = BasicTh(params=params)
-    for i in range(100):
-        model.run_one_step(dt)
-
-    # construct actual and predicted slopes
-    # note that since we have a smooth threshold, we do not have a true
-    # analytical solution, but a bracket within wich we expect the actual slopes
-    # to fall.
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    predicted_slopes_upper = ((U + threshold) / (K * (actual_areas ** m))) ** (1. / n)
-    predicted_slopes_lower = ((U + 0.0) / (K * (actual_areas ** m))) ** (1. / n)
-
-    # assert actual and predicted slopes are in the correct range for the slopes.
-    assert np.all(actual_slopes[model.grid.core_nodes[1:-1]] > predicted_slopes_lower[model.grid.core_nodes[1:-1]]) == True
-    assert np.all(actual_slopes[model.grid.core_nodes[1:-1]] < predicted_slopes_upper[model.grid.core_nodes[1:-1]]) == True
 
 
 def test_steady_Ksp_no_precip_changer():
@@ -163,7 +108,7 @@ def test_steady_Ksp_no_precip_changer_with_depression_finding():
     assert_array_almost_equal(actual_slopes[model.grid.core_nodes[1:-1]],
                               predicted_slopes[model.grid.core_nodes[1:-1]],
                               decimal=4)
-    
+
 def test_with_precip_changer():
     K = 0.01
     threshold = 0.000001
@@ -194,7 +139,7 @@ def test_with_precip_changer():
     model.run_one_step(1.0)
     model.run_one_step(1.0)
     assert round(model.eroder.K, 5) == 0.10326
-    
+
 def test_diffusion_only():
     total_time = 5.0e6
     U = 0.001
