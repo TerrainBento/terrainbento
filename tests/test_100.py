6637d0efc6f3bb91f26385393c8517251c7c3deb
Greg Tucker <gtucker@colorado.edu>
2018-07-20 17:23:29 -0600

    add diff only test

--- tests/test_100.py
+++ tests/test_100.py
@@ -155,51 +155,56 @@ def test_stochastic_duration_rainfall_means():
 
 
 
-#def test_diffusion_only():
-#    total_time = 5.0e6
-#    U = 0.001
-#    D = 1
-#    m = 0.75
-#    n = 1.0
-#    dt =1000
-#
-#    # construct dictionary. note that D is turned off here
-#    params = {'model_grid': 'RasterModelGrid',
-#              'dt': 1,
-#              'output_interval': 2.,
-#              'run_duration': 200.,
-#              'number_of_node_rows' : 3,
-#              'number_of_node_columns' : 21,
-#              'node_spacing' : 100.0,
-#              'north_boundary_closed': True,
-#              'west_boundary_closed': False,
-#              'south_boundary_closed': True,
-#              'regolith_transport_parameter': D,
-#              'water_erodability': 0.0,
-#              'm_sp': m,
-#              'n_sp': n,
-#              'random_seed': 3141,
-#              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
-#              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
-#                                              'lowering_rate': -U}}
-#    nts = int(total_time/dt)
-#
-#    reference_node = 9
-#    # construct and run model
-#    model = Basic(params=params)
-#    for i in range(nts):
-#        model.run_one_step(dt)
-#
-#
-#    predicted_z = (model.z[model.grid.core_nodes[reference_node]]-(U / (2. * D)) *
-#               ((model.grid.x_of_node - model.grid.x_of_node[model.grid.core_nodes[reference_node]])**2))
-#
-#    # assert actual and predicted elevations are the same.
-#    assert_array_almost_equal(predicted_z[model.grid.core_nodes],
-#                              model.z[model.grid.core_nodes],
-#                              decimal=2)
-#
-#
+def test_diffusion_only():
+    total_time = 5.0e6
+    U = 0.001
+    D = 1
+    m = 0.75
+    n = 1.0
+    dt = 1000
+
+    # construct dictionary. note that D is turned off here
+    params = {'model_grid': 'RasterModelGrid',
+              'dt': 1,
+              'output_interval': 2.,
+              'run_duration': 200.,
+              'number_of_node_rows' : 3,
+              'number_of_node_columns' : 21,
+              'node_spacing' : 100.0,
+              'north_boundary_closed': True,
+              'west_boundary_closed': False,
+              'south_boundary_closed': True,
+              'regolith_transport_parameter': D,
+              'water_erodability~stochastic': 0.0,
+              'm_sp': m,
+              'n_sp': n,
+              'number_of_sub_time_steps': 100,
+              'infiltration_capacity': 1.0,
+              'daily_rainfall_intermittency_factor': 1.0,
+              'daily_rainfall__mean_intensity': 1.0,
+              'daily_rainfall__precipitation_shape_factor': 1.0,
+              'random_seed': 3141,
+              'BoundaryHandlers': 'NotCoreNodeBaselevelHandler',
+              'NotCoreNodeBaselevelHandler': {'modify_core_nodes': True,
+                                              'lowering_rate': -U}}
+    nts = int(total_time/dt)
+
+    reference_node = 9
+    # construct and run model
+    model = BasicSt(params=params)
+    for i in range(nts):
+        model.run_one_step(dt)
+
+
+    predicted_z = (model.z[model.grid.core_nodes[reference_node]]-(U / (2. * D)) *
+               ((model.grid.x_of_node - model.grid.x_of_node[model.grid.core_nodes[reference_node]])**2))
+
+    # assert actual and predicted elevations are the same.
+    assert_array_almost_equal(predicted_z[model.grid.core_nodes],
+                              model.z[model.grid.core_nodes],
+                              decimal=2)
+
+
 #def test_with_precip_changer():
 #    K =  0.01
 #    params = {'model_grid': 'RasterModelGrid',
