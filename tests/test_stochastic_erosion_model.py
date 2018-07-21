35f7394bc96ef1aa3682f9f18b6bb377f38f493f
kbarnhart <katy.barnhart@gmail.com>
2018-07-20 18:47:55 -0600

    first pass at stochastic tests.

--- tests/test_stochastic_erosion_model.py
+++ tests/test_stochastic_erosion_model.py
@@ -1,16 +1,63 @@
 # coding: utf8
 #! /usr/env/python
 
-from landlab import HexModelGrid
+import pytest
+
 from terrainbento import StochasticErosionModel, BasicSt
+
+def test_defaults():
+    params = {
+        "dt": 1,
+        "output_interval": 2.,
+        "run_duration": 200.}
+    model = StochasticErosionModel(params=params)
+    assert model.opt_stochastic_duration == False
+    assert model.record_rain == False
 
 
 def test_run_opt_true():
-    pass
+    params = {
+        "dt": 1,
+        "output_interval": 2.,
+        "run_duration": 200.,
+        "record_rain": True
+    }
+    model = StochasticErosionModel(params=params)
+    assert model.record_rain == True
+    assert isinstance(model.rain_record, dict)
+    fields = ["event_start_time", "event_duration", "rainfall_rate", "runoff_rate"]
+    for f in fields:
+        assert f in model.rain_record
+        assert len(model.rain_record[f]) == 0
 
 
 def test_run_opt_false():
-    pass
+    params = {
+        "dt": 1,
+        "output_interval": 2.,
+        "run_duration": 200.,
+        "record_rain": False
+    }
+    model = StochasticErosionModel(params=params)
+    assert model.record_rain == False
+    assert model.rain_record is None
+
+
+def test_freq_file_with_opt_duration_true():
+    params = {
+        "model_grid": "RasterModelGrid",
+        "dt": 1,
+        "output_interval": 2.,
+        "run_duration": 200.,
+        "number_of_node_rows": 3,
+        "number_of_node_columns": 20,
+        "node_spacing": 100.0,
+        "random_seed": 3141,
+        "frequency_filename": "yams.txt",
+        "opt_stochastic_duration": True
+    }
+    with pytest.raises(ValueError):
+        _ = StochasticErosionModel(params=params)
 
 
 def test_run_opt_true_with_changer():
@@ -50,11 +97,6 @@ def test_write_exceedance_frequency_file():
 
 def test_not_specifying_record_rain():
     pass
-
-
-def test_opt_true_freq_file():
-    pass
-    # this raises a value error
 
 
 def test_write_files_no_record():
