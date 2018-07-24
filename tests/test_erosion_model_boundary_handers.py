5ca6dedb609e8923c3a10f7c7519518c48ea558d
kbarnhart <katy.barnhart@gmail.com>
2018-07-24 11:40:03 -0600

    two boundary handler test

--- tests/test_erosion_model_boundary_handers.py
+++ tests/test_erosion_model_boundary_handers.py
@@ -129,4 +129,34 @@ def test_pass_boundary_handlers_as_instance():
 
 
 def test_pass_two_boundary_handlers():
-    pass
+    U = 0.0001
+    K = 0.001
+    m = 1. / 3.
+    n = 2. / 3.
+    # construct dictionary. note that D is turned off here
+    params = {
+        "model_grid": "RasterModelGrid",
+        "dt": 1,
+        "output_interval": 2.,
+        "run_duration": 200.,
+        "number_of_node_rows": 3,
+        "number_of_node_columns": 20,
+        "node_spacing": 100.0,
+        "north_boundary_closed": True,
+        "south_boundary_closed": True,
+        "regolith_transport_parameter": 0.,
+        "water_erodability": K,
+        "m_sp": m,
+        "n_sp": n,
+        "random_seed": 3141,
+        "BoundaryHandlers": ["NotCoreNodeBaselevelHandler", "SingleNodeBaselevelHandler"],
+        "NotCoreNodeBaselevelHandler": {"modify_core_nodes": True, "lowering_rate": -U},
+        "SingleNodeBaselevelHandler":{"lowering_rate": -U}
+    }
+    model = Basic(params=params)
+    model.run_one_step(1.0)
+
+    truth = np.zeros(model.z.size)
+    truth[0] -= U
+    truth[model.grid.core_nodes] += U
+    assert_array_equal(model.z, truth)
