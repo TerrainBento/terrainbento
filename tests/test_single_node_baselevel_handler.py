12eb0b461fdb157f11983b0688ad415a2b1a4464
kbarnhart <katy.barnhart@gmail.com>
2018-07-24 14:49:53 -0600

    test hex typos

--- tests/test_single_node_baselevel_handler.py
+++ tests/test_single_node_baselevel_handler.py
@@ -10,7 +10,7 @@ from landlab import RasterModelGrid, HexModelGrid
 _TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 
-def text_hex():
+def test_hex():
     "Test using a hex grid"
 
     mg = HexModelGrid(5, 5)
