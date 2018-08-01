4cdae06282bb947ed0ca6a551da5d900fdae1e1c
cmshobe <cmshobe@email.wm.edu>
2018-08-01 13:59:01 -0600

    fixing param definitions in basicHyVs

--- terrainbento/derived_models/model_210_basicHyVs.py
+++ terrainbento/derived_models/model_210_basicHyVs.py
@@ -40,9 +40,8 @@ class BasicHyVs(ErosionModel):
     :math:`K` is the erodability by water, :math:`\omega_c` is the critical
     stream power needed for erosion to occur, :math:`V` is effective sediment
     settling velocity, :math:`Q_s` is volumetric sediment flux, :math:`Q` is
-    volumetric water discharge, :math:`\phi` is sediment porosity, :math:`D` is
-    the regolith transport efficiency, :math:`H` is soil depth, and :math:`H_*`
-    is the bedrock roughness length scale.
+    volumetric water discharge, :math:`\phi` is sediment porosity, and
+    :math:`D` is the regolith transport efficiency.
 
     :math:`\\alpha` is the saturation area scale used for transforming area into
     effective area :math:`A_{eff}`. It is given as a function of the saturated
