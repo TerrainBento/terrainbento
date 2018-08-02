91a687b72058a242037c7eac96614d288284c27e
kbarnhart <katy.barnhart@gmail.com>
2018-08-01 19:58:22 -0600

    adding test for one field

--- tests/test_erosion_model_outputs.py
+++ tests/test_erosion_model_outputs.py
@@ -45,6 +45,7 @@ def test_write_output_hex():
 
     model.remove_output_netcdfs()
 
+
 def test_write_synthesis_netcdf():
     fp = os.path.join(_TEST_DATA_DIR, "basic_raster_inputs_for_nc.txt")
     truth = os.path.join(_TEST_DATA_DIR, "truth.nc" )
@@ -54,6 +55,30 @@ def test_write_synthesis_netcdf():
     ds = model.to_xarray_dataset(time_unit='years', space_unit='meter')
 
     out_fn = "tb_output.nc"
+    model.save_to_xarray_dataset(filename=out_fn, time_unit='years', space_unit='meter')
+
+    output = xr.open_dataset(out_fn, decode_times=False)
+    truth = xr.open_dataset(truth, decode_times=False)
+
+    assert truth.dims == output.dims
+    assert truth.dims == ds.dims
+
+    assert truth.equals(output) == True
+    assert truth.equals(ds) == True
+
+    model.remove_output_netcdfs()
+    os.remove(out_fn)
+
+
+def test_write_synthesis_netcdf():
+    fp = os.path.join(_TEST_DATA_DIR, "basic_raster_inputs.txt")
+    truth = os.path.join(_TEST_DATA_DIR, "truth_one_field.nc" )
+    model = Basic(input_file=fp)
+    model.run(output_fields="topographic__elevation")
+
+    ds = model.to_xarray_dataset(time_unit='years', space_unit='meter')
+
+    out_fn = "tb_output_one_field.nc"
     model.save_to_xarray_dataset(filename=out_fn, time_unit='years', space_unit='meter')
 
     output = xr.open_dataset(out_fn, decode_times=False)
