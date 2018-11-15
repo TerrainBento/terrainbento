6bc345562a8b3ea40f6ad62e45bc33c62dfa82c0
kbarnhart <katy.barnhart@gmail.com>
2018-11-15 07:53:19 -0700

    wip on runoff coeff for VSA

--- terrainbento/runoff_generators/vsa_runoff.py
+++ terrainbento/runoff_generators/vsa_runoff.py
@@ -14,39 +14,24 @@ class VariableSourceAreaRunoff(object):
         self.p =  mg.at_node['rainfall__flux']
         self.r = mg.at_node['water__unit_flux_in']
 
-        self.area = mg.
+        self.area = mg.at_node['drainage_area']
         self.slope = mg.at_node['topographic_steepest_slope']
 
-        self.soil__initial_thickness = (self._length_factor) * self.params[
-            "soil__initial_thickness"
-        ]  # has units length
+        self.H = mg.at_node['soil__depth']
 
         self.K_hydraulic_conductivity = (self._length_factor) * self.params[
             "hydraulic_conductivity"
         ]  # has units length per time
 
-        # Get the transmissivity parameter
-        # transmissivity is hydraulic condiuctivity times soil thickness
-        self.trans = self.K_hydraulic_conductivity * self.soil__initial_thickness
-
-        if self.trans <= 0.0:
-            raise ValueError("BasicStVs: Transmissivity must be > 0")
-
-        self.tlam = self.trans * self.grid._dx  # assumes raster
-
     def run_one_step(self, dt):
         """
         """
-        pass
+        # Get the transmissivity parameter
+        # transmissivity is hydraulic condiuctivity times soil thickness
+        self.tlam = self.K_hydraulic_conductivity * self.H * self.grid._dx
+        if np.any(self.tlam) <= 0.0:
+            raise ValueError("VSA Runoff: Transmissivity must be > 0")
 
-        # Here"s the total (surface + subsurface) discharge
-        pa = self.rain_rate * self.area
+        a = self.tlam * self.slope / self.p
 
-        # Transmissivity x lambda x slope = subsurface discharge capacity
-        tls = self.tlam * self.slope[np.where(self.slope > 0.0)[0]]
-
-        # Subsurface discharge: zero where slope is flat
-        self.qss[np.where(self.slope <= 0.0)[0]] = 0.0
-        self.qss[np.where(self.slope > 0.0)[0]] = tls * (
-            1.0 - np.exp(-pa[np.where(self.slope > 0.0)[0]] / tls)
-        )
+        r = (a / self.A**2.)  * np.exp(-a / self.A)
