5a4bb01517886995ed6a4f715236132edb3ef7ac
kbarnhart <katy.barnhart@gmail.com>
2019-01-09 11:22:46 -0700

    passes

--- tests/test_model_basicHyRt.py
+++ tests/test_model_basicHyRt.py
@@ -1,142 +1,62 @@
-import os
-
+# coding: utf8
+# !/usr/env/python
 import numpy as np
+import pytest
 from numpy.testing import assert_array_almost_equal
 
-from terrainbento import BasicHyRt
-from terrainbento.utilities import filecmp
-
-_TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+from terrainbento import BasicHyRt, NotCoreNodeBaselevelHandler
 
 
-def test_steady_Ksp_no_precip_changer():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 0.5
-    n = 1.0
-    step = 10
-    v_sc = 0.001
+@pytest.mark.parametrize("m_sp,n_sp", [(1. / 3, 2. / 3.), (0.5, 1.0)])
+@pytest.mark.parametrize(
+    "depression_finder", [None, "DepressionFinderAndRouter"]
+)
+@pytest.mark.parametrize("solver", ["basic"])
+def test_no_precip_changer(
+    clock_simple, grid_2, m_sp, n_sp, depression_finder, U, solver
+):
+    ncnblh = NotCoreNodeBaselevelHandler(
+        grid_2, modify_core_nodes=True, lowering_rate=-U
+    )
     phi = 0.1
     F_f = 0.0
-
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.asc")
+    v_sc = 0.001
+    Kr = 0.001
+    Kt = 0.005
     # construct dictionary. note that D is turned off here
     params = {
-        "model_grid": "RasterModelGrid",
+        "grid": grid_2,
         "clock": clock_simple,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
         "regolith_transport_parameter": 0.,
-        "water_erodability_lower": Kr,
         "water_erodability_upper": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
+        "water_erodability_lower": Kr,
         "settling_velocity": v_sc,
         "sediment_porosity": phi,
         "fraction_fines": F_f,
-        "solver": "basic",
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
+        "solver": solver,
+        "m_sp": m_sp,
+        "n_sp": n_sp,
+        "depression_finder": depression_finder,
+        "boundary_handlers": {"NotCoreNodeBaselevelHandler": ncnblh},
     }
 
-    model = BasicHyRt(params=params)
-    for _ in range(2000):
-        model.run_one_step(step)
+    # construct and run model
+    model = BasicHyRt(**params)
+    for _ in range(3000):
+        model.run_one_step(10)
 
     # construct actual and predicted slopes
     actual_slopes = model.grid.at_node["topographic__steepest_slope"]
     actual_areas = model.grid.at_node["drainage_area"]
     rock_predicted_slopes = np.power(
-        ((U * v_sc) / (Kr * np.power(actual_areas, m)))
-        + (U / (Kr * np.power(actual_areas, m))),
-        1. / n,
+        ((U * v_sc) / (Kr * np.power(actual_areas, m_sp)))
+        + (U / (Kr * np.power(actual_areas, m_sp))),
+        1. / n_sp,
     )
     till_predicted_slopes = np.power(
-        ((U * v_sc) / (Kt * np.power(actual_areas, m)))
-        + (U / (Kt * np.power(actual_areas, m))),
-        1. / n,
-    )
-
-    # assert actual and predicted slopes are the same for rock and till
-    # portions.
-    assert_array_almost_equal(
-        actual_slopes[22:37], rock_predicted_slopes[22:37]
-    )
-
-    # assert actual and predicted slopes are the same for rock and till
-    # portions.
-    assert_array_almost_equal(
-        actual_slopes[82:97], till_predicted_slopes[82:97]
-    )
-
-
-def test_steady_Ksp_no_precip_changer_with_depression_finding():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 1. / 3.
-    n = 2. / 3.
-    step = 10
-    v_sc = 0.001
-    phi = 0.1
-    F_f = 0.0
-
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.asc")
-    # construct dictionary. note that D is turned off here
-    params = {
-        "model_grid": "RasterModelGrid",
-        "clock": clock_simple,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
-        "regolith_transport_parameter": 0.,
-        "water_erodability_lower": Kr,
-        "water_erodability_upper": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "settling_velocity": v_sc,
-        "sediment_porosity": phi,
-        "fraction_fines": F_f,
-        "solver": "basic",
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "depression_finder": "DepressionFinderAndRouter",
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
-    }
-
-    model = BasicHyRt(params=params)
-    for _ in range(2000):
-        model.run_one_step(step)
-
-    # construct actual and predicted slopes
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = np.power(
-        ((U * v_sc) / (Kr * np.power(actual_areas, m)))
-        + (U / (Kr * np.power(actual_areas, m))),
-        1. / n,
-    )
-    till_predicted_slopes = np.power(
-        ((U * v_sc) / (Kt * np.power(actual_areas, m)))
-        + (U / (Kt * np.power(actual_areas, m))),
-        1. / n,
+        ((U * v_sc) / (Kt * np.power(actual_areas, m_sp)))
+        + (U / (Kt * np.power(actual_areas, m_sp))),
+        1. / n_sp,
     )
 
     # assert actual and predicted slopes are the same for rock and till
