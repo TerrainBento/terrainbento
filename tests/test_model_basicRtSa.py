226fecff957fed2d0ce2b911bc8a36d1f7278ae0
kbarnhart <katy.barnhart@gmail.com>
2019-01-08 18:43:59 -0700

    wip

--- tests/test_model_basicRtSa.py
+++ tests/test_model_basicRtSa.py
@@ -3,249 +3,42 @@ import os
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
-from terrainbento import BasicRtSa
-from terrainbento.utilities import filecmp
-
-_TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+from terrainbento import BasicRtSa, NotCoreNodeBaselevelHandler
 
 
-def test_steady_Kss_no_precip_changer():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 1. / 3.
-    n = 2. / 3.
-    step = 1000
-    max_soil_production_rate = 0.0
-    soil_production_decay_depth = 0.2
-    soil_transport_decay_depth = 0.5
-    soil__initial_thickness = 0.
+def test_diffusion_only(clock_simple, grid_4):
 
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.asc")
-    # construct dictionary. note that D is turned off here
-    params = {
-        "model_grid": "RasterModelGrid",
-        "clock": clock_simple,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
-        "regolith_transport_parameter": 0.,
-        "soil_transport_decay_depth": soil_transport_decay_depth,
-        "soil_production__maximum_rate": max_soil_production_rate,
-        "soil_production__decay_depth": soil_production_decay_depth,
-        "soil__initial_thickness": soil__initial_thickness,
-        "water_erodability_lower": Kr,
-        "water_erodability_upper": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
-    }
-
-    # construct and run model
-    model = BasicRtSa(params=params)
-    for _ in range(100):
-        model.run_one_step(step)
-
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[22:37], rock_predicted_slopes[22:37]
-    )
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[82:97], till_predicted_slopes[82:97]
-    )
-
-
-def test_steady_Ksp_no_precip_changer():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 0.5
-    n = 1.0
-    step = 1000
-    max_soil_production_rate = 0.0
-    soil_production_decay_depth = 0.2
-    soil_transport_decay_depth = 0.5
-    soil__initial_thickness = 0.
-
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.asc")
-    # construct dictionary. note that D is turned off here
-    params = {
-        "model_grid": "RasterModelGrid",
-        "clock": clock_simple,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
-        "regolith_transport_parameter": 0.,
-        "soil_transport_decay_depth": soil_transport_decay_depth,
-        "soil_production__maximum_rate": max_soil_production_rate,
-        "soil_production__decay_depth": soil_production_decay_depth,
-        "soil__initial_thickness": soil__initial_thickness,
-        "water_erodability_lower": Kr,
-        "water_erodability_upper": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
-    }
-
-    # construct and run model
-    model = BasicRtSa(params=params)
-    for _ in range(100):
-        model.run_one_step(step)
-
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[22:37], rock_predicted_slopes[22:37]
-    )
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[82:97], till_predicted_slopes[82:97]
-    )
-
-
-def test_steady_Ksp_no_precip_changer_with_depression_finding():
-    U = 0.0001
-    Kr = 0.001
-    Kt = 0.005
-    m = 0.5
-    n = 1.0
-    step = 1000
-    max_soil_production_rate = 0.0
-    soil_production_decay_depth = 0.2
-    regolith_transport_parameter = 0.0
-    soil_transport_decay_depth = 0.5
-    soil__initial_thickness = 0.
-
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_unit.asc")
-    # construct dictionary. note that D is turned off here
-    params = {
-        "model_grid": "RasterModelGrid",
-        "clock": clock_simple,
-        "number_of_node_rows": 8,
-        "number_of_node_columns": 20,
-        "node_spacing": 100.0,
-        "north_boundary_closed": True,
-        "south_boundary_closed": True,
-        "regolith_transport_parameter": 0.,
-        "soil_transport_decay_depth": soil_transport_decay_depth,
-        "soil_production__maximum_rate": max_soil_production_rate,
-        "soil_production__decay_depth": soil_production_decay_depth,
-        "soil__initial_thickness": soil__initial_thickness,
-        "water_erodability_lower": Kr,
-        "water_erodability_upper": Kt,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "depression_finder": "DepressionFinderAndRouter",
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
-    }
-
-    # construct and run model
-    model = BasicRtsSa(params=params)
-    for _ in range(100):
-        model.run_one_step(step)
-
-    actual_slopes = model.grid.at_node["topographic__steepest_slope"]
-    actual_areas = model.grid.at_node["drainage_area"]
-    rock_predicted_slopes = (U / (Kr * (actual_areas ** m))) ** (1. / n)
-    till_predicted_slopes = (U / (Kt * (actual_areas ** m))) ** (1. / n)
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[22:37], rock_predicted_slopes[22:37]
-    )
-
-    # assert actual and predicted slopes are the same for rock and till.
-    assert_array_almost_equal(
-        actual_slopes[82:97], till_predicted_slopes[82:97]
-    )
-
-
-def test_diffusion_only():
     U = 0.001
-    m = 0.5
-    n = 1.0
-    step = 10.
-    dx = 10.
     max_soil_production_rate = 0.002
     soil_production_decay_depth = 0.2
     regolith_transport_parameter = 1.0
     soil_transport_decay_depth = 0.5
-    initial_soil_thickness = 0.0
     number_of_node_rows = 21
 
-    # construct dictionary. note that D is turned off here
-    file_name = os.path.join(_TEST_DATA_DIR, "example_contact_diffusion.asc")
-    # construct dictionary. note that D is turned off here
+    ncnblh = NotCoreNodeBaselevelHandler(
+        grid_4, modify_core_nodes=True, lowering_rate=-U
+    )
+
     params = {
-        "model_grid": "RasterModelGrid",
-        "clock": {"step": step, "output_interval": 2., "stop": 200.},
-        "number_of_node_rows": number_of_node_rows,
-        "number_of_node_columns": 3,
-        "node_spacing": dx,
-        "east_boundary_closed": True,
-        "west_boundary_closed": True,
+        "grid": grid_4,
+        "clock": clock_simple,
         "regolith_transport_parameter": regolith_transport_parameter,
         "soil_transport_decay_depth": soil_transport_decay_depth,
         "soil_production__maximum_rate": max_soil_production_rate,
         "soil_production__decay_depth": soil_production_decay_depth,
-        "soil__initial_thickness": initial_soil_thickness,
         "water_erodability_lower": 0,
         "water_erodability_upper": 0,
-        "lithology_contact_elevation__file_name": file_name,
-        "contact_zone__width": 1.,
-        "m_sp": m,
-        "n_sp": n,
-        "random_seed": 3141,
-        "BoundaryHandlers": "NotCoreNodeBaselevelHandler",
-        "NotCoreNodeBaselevelHandler": {
-            "modify_core_nodes": True,
-            "lowering_rate": -U,
-        },
+        "boundary_handlers": {"NotCoreNodeBaselevelHandler": ncnblh},
     }
 
     # construct and run model
-    model = BasicRtSa(params=params)
+    model = BasicRtSa(**params)
     for _ in range(120000):
-        model.run_one_step(step)
+        model.run_one_step(10)
 
-    # test steady state soil depth
+    dx = grid_4.dx
+
+    # test steady state soil depthf
     actual_depth = model.grid.at_node["soil__depth"][28]
     predicted_depth = -soil_production_decay_depth * np.log(
         U / max_soil_production_rate
