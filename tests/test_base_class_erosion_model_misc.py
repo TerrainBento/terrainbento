850e133f01032f6de2079fbce148572cf60f9dc6
kbarnhart <katy.barnhart@gmail.com>
2019-01-09 17:34:24 -0700

    misc passes!

--- tests/test_base_class_erosion_model_misc.py
+++ tests/test_base_class_erosion_model_misc.py
@@ -3,13 +3,13 @@
 
 import numpy as np
 import pytest
+from landlab import RasterModelGrid
 
 from terrainbento import ErosionModel
 
 
-def test_calc_cumulative_erosion(clock_simple):
-    params = {"model_grid": "HexModelGrid", "clock": clock_simple}
-    em = ErosionModel(params=params)
+def test_calc_cumulative_erosion(clock_simple, grid_1):
+    em = ErosionModel(grid=grid_1, clock=clock_simple)
     assert np.array_equiv(em.z, 0.) is True
     em.z += 1.
     em.calculate_cumulative_change()
