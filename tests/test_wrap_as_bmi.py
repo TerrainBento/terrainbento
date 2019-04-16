8f559737448861101616aaafbdbb4d3625577e3f
kbarnhart <katy.barnhart@gmail.com>
2019-04-16 08:04:34 -0600

    notes on eventual tests and questions for Eric and Mark on github

--- tests/test_wrap_as_bmi.py
+++ tests/test_wrap_as_bmi.py
@@ -8,11 +8,18 @@ from terrainbento.derived_models import MODELS
 
 @pytest.mark.parametrize("Model", MODELS)
 def test_wrap_as_bmi(Model):
-    # test something
 
     # verify  that all models BMI attributes are the correct values in the
     # docstring information.
 
+    # verify that all the grid information is correct.
+
+    # verify that the model can run as expected.
+
     # verify that all units are either None or UDUNITS compatible.
 
     pass
+
+
+# TODO: ask questions on github about: information about parameter values and ranges.
+# BMI expectations about file writing.
