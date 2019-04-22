2bb7a4a7f34c79a4d78da1f86101cfa87c786588
kbarnhart <katy.barnhart@gmail.com>
2019-04-22 07:49:03 -0600

    changing how grid is used.

--- terrainbento/bmi/bmi.py
+++ terrainbento/bmi/bmi.py
@@ -7,15 +7,11 @@ from .info import BmiInfo
 from .time import BmiTime
 from .vars import BmiVars
 from .getter_setter import BmiGetter, BmiSetter
-from .grid_rectilinear import BmiGridRectilinear
-from .grid_uniform_rectilinear import BmiGridUniformRectilinear
-from .grid_structured_quad import BmiGridStructuredQuad
-from .grid_unstructured import BmiGridUnstructured
+from .grid import BmiGrid
 
 
 class BmiModel(Bmi, BmiBase, BmiInfo, BmiTime, BmiVars, BmiGetter, BmiSetter,
-          BmiGridRectilinear, BmiGridUniformRectilinear, BmiGridStructuredQuad,
-          BmiGridUnstructured):
+          BmiGrid):
 
     """The complete Basic Model Interface for terrainbento.
 
