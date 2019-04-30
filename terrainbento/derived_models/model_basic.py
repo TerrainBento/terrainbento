61f8bcd1f560b691a9a3a355c2ed514ac4219bbc
kbarnhart <katy.barnhart@gmail.com>
2019-04-30 15:23:36 -0600

    wip regarding standard values.

--- terrainbento/derived_models/model_basic.py
+++ terrainbento/derived_models/model_basic.py
@@ -51,14 +51,14 @@ class Basic(ErosionModel):
             "type": float,
             "units": "m",
             "at": "node",
-            "definition": "Land surface topographic elevation",
+            "long_name": "Land surface topographic elevation",
         },
         "water__unit_flux_in": {
             "type": float,
             "units": "m/s",
             "at": "node",
-            "definition": (
-                "Volume water per area per time input to each " "node."
+            "long_name": (
+                "Volume water per area per time input to each node."
             ),
         },
     }
@@ -69,6 +69,7 @@ class Basic(ErosionModel):
             "units": None,  # should I put in dependence on M?
             "default": 0.001,  # these values assume yr instead of second
             "range": [1e-7, 1e-2],  # these values assume yr instead of second
+            "long_name": "",
             "definition": (
                 "Water erodibility coefficient. Units depend on "
                 "values of m_sp and n_sp."
@@ -79,6 +80,7 @@ class Basic(ErosionModel):
             "units": "m/(s^2)",
             "default": 0.1,  # these values assume yr instead of second
             "range": [1e-3, 1e-1],  # these values assume yr instead of second
+            "long_name": "",
             "definition": ("Hillslope diffusivity."),
         },
     }
