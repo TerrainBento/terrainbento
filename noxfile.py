ba84e7692601226e717d6a6de2838b78e1e3fa71
Mark Piper <mark.piper@colorado.edu>
2024-07-17 14:43:10 -0600

    Use nox for build, test, release, and publish tasks (#166)
    * Create basic pyproject.toml file
    
    * Move configuration settings to pyproject.toml
    
    * Remove setuptools files
    
    Move flake8 config to .flake8.
    
    * Tabs to spaces
    
    * Install with pip
    
    * Add package data to the manifest template
    
    Setuptools still prefers a manifest template: https://setuptools.pypa.io/en/latest/userguide/datafiles.html#summary
    
    * Include version in package definition
    
    * Include noxfile.py in manifest template
    
    * Remove Makefile in favor of noxfile.py
    
    * Nox is the only dev tool needed
    
    * Add and group requirements
    
    * Start a noxfile
    
    * Add build, release, and publish sessions

---
+++ noxfile.py
@@ -0,0 +1,106 @@
+import os
+import pathlib
+import shutil
+from itertools import chain
+
+import nox
+
+PROJECT = "terrainbento"
+HERE = pathlib.Path(__file__)
+ROOT = HERE.parent
+PATH = {
+    "build": ROOT / "build",
+    "dist": ROOT / "dist",
+    "docs": ROOT / "docs",
+    "notebooks": ROOT / "notebooks",
+    "nox": pathlib.Path(".nox"),
+    "root": ROOT,
+}
+PYTHON_VERSION = "3.12"
+
+
+@nox.session(python=PYTHON_VERSION)
+def test(session: nox.Session) -> None:
+    """Run the tests."""
+    session.install(".[testing,notebooks]")
+
+    args = [
+        "--cov",
+        PROJECT,
+        "-vvv",
+    ] + session.posargs
+
+    if "CI" in os.environ:
+        args.append(f"--cov-report=xml:{ROOT.absolute()!s}/coverage.xml")
+    session.run("pytest", *args)
+
+    if "CI" not in os.environ:
+        session.run("coverage", "report", "--ignore-errors", "--show-missing")
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """Build source and binary distributions."""
+    session.install(".[build]")
+    session.run("python", "-m", "build", "--outdir", PATH["dist"])
+    session.run("twine", "check", PATH["dist"] / "*")
+
+
+@nox.session
+def release(session):
+    """Tag, build and publish a new release to PyPI."""
+    session.install(".[build]")
+    session.run("fullrelease")
+
+
+@nox.session(name="testpypi")
+def publish_testpypi(session):
+    """Upload package to TestPyPI."""
+    build(session)
+    session.run(
+        "twine",
+        "upload",
+        "--skip-existing",
+        "--repository-url",
+        "https://test.pypi.org/legacy/",
+        PATH["dist"] / "*",
+    )
+
+
+@nox.session(name="pypi")
+def publish_pypi(session):
+    """Upload package to PyPI."""
+    build(session)
+    session.run(
+        "twine",
+        "upload",
+        "--skip-existing",
+        "--repository-url",
+        "https://upload.pypi.org/legacy/",
+        PATH["dist"] / "*",
+    )
+
+
+@nox.session(python=False)
+def clean(session):
+    """Remove virtual environments, build files, and caches."""
+    shutil.rmtree("build", ignore_errors=True)
+    shutil.rmtree("dist", ignore_errors=True)
+    shutil.rmtree("docs/build", ignore_errors=True)
+    shutil.rmtree(f"{PROJECT}.egg-info", ignore_errors=True)
+    shutil.rmtree(".pytest_cache", ignore_errors=True)
+    shutil.rmtree(".venv", ignore_errors=True)
+    if os.path.exists(".coverage"):
+        os.remove(".coverage")
+    for p in chain(ROOT.rglob("*.py[co]"), ROOT.rglob("__pycache__")):
+        if p.is_dir():
+            p.rmdir()
+        else:
+            p.unlink()
+
+
+@nox.session(python=False)
+def nuke(session):
+    """Clean and also remove the .nox/ directory."""
+    clean(session)
+    shutil.rmtree(".nox", ignore_errors=True)
